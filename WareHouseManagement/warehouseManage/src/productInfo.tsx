9b3477ec61701ebbaf03f2829dc1ec3f768de6ae
gurlalbadwal <gbad0004@student.monash.edu>
2025-12-23 09:17:24 +0100

    fully done

--- WareHouseManagement/warehouseManage/src/productInfo.tsx
+++ WareHouseManagement/warehouseManage/src/productInfo.tsx
@@ -1,7 +1,9 @@
-import { collection, getDocs, query, where, type Timestamp, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, query, where, type Timestamp, doc, getDoc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { db } from "./firebase/config";
+import { auth, db } from "./firebase/config";
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from "@mui/icons-material/Check";
 // import { useState } from "react";
 // import { useParams } from "react-router-dom";
 
@@ -13,10 +15,22 @@ type ProductAddInfo = {
     id: string, createdAt: Timestamp, modifiedAt: Timestamp, addedBy: string, modifiedBy: string, remarks: string,
 }
 
+type User = {
+    email: string;
+    role: string;
+}
+
 function ProductInfo(){
     const {id} = useParams<{id: string}>();
     const [removed, setRemoved] = useState<ProductRemoveInfoType[]>([]);
     const [proInfo, setProInfo] = useState<ProductAddInfo | null>(null);
+    const [editingId, setEditingId] = useState<string | null>(null);
+    const [editBill, setEditBill] = useState(0);
+    const [editKTN, setEditKTN] = useState(0);
+    const [users, setUsers] = useState<User[]>([]);
+    const currentUserEmail = auth.currentUser?.email;
+    const currentUserRole = users.find(u => u.email === currentUserEmail)?.role;
+    
     
     useEffect(() => {
         const fetchRemoveInfo = async () => {
@@ -28,10 +42,17 @@ function ProductInfo(){
             const docSnap = await getDocs(q);
             const docSnapqq = await getDoc(qq);
 
-            const list = docSnap.docs.map(d => ({
-                id: d.id,
-                ...(d.data() as Omit<ProductRemoveInfoType, "id">)
-            }));
+            setRemoved(
+                docSnap.docs.map(d => ({
+                    id: d.id,
+                    ...(d.data() as Omit<ProductRemoveInfoType, "id">)
+                }))
+            );
+
+            // const list = docSnap.docs.map(d => ({
+            //     id: d.id,
+            //     ...(d.data() as Omit<ProductRemoveInfoType, "id">)
+            // }));
 
             if(docSnapqq.exists()){
                 setProInfo({
@@ -42,11 +63,46 @@ function ProductInfo(){
                 setProInfo(null);
             }
 
-            setRemoved(list);
+            // setRemoved(list);
         };
 
         fetchRemoveInfo();
     }, []);
+
+    useEffect(() => {
+            async function loadUsers(){
+                const querySnapshot = await getDocs(collection(db, "WMSUsers"));
+                const list: User[] = [];
+    
+                querySnapshot.forEach((doc) =>{
+                    list.push(doc.data() as User);
+                });
+                setUsers(list);
+            }
+            loadUsers();
+        }, []);
+
+    const startEdit = (r : ProductRemoveInfoType) => {
+        setEditingId(r.id);
+        setEditBill(r.bill_no);
+        setEditKTN(r.ktns);
+    };
+
+    const saveEdit = async (rowId: string) => {
+        await updateDoc(doc(db, "WMSRemoveInfo", rowId), {
+            bill_no: editBill,
+            ktns: editKTN,
+        });
+
+        setRemoved(prev =>
+        prev.map(r =>
+            r.id === rowId ? { ...r, bill_no: editBill, ktns: editKTN } : r
+            )
+        );
+        setEditingId(null);
+    };
+
+
 
     return(
         <div className="container mt-4">
@@ -66,15 +122,45 @@ function ProductInfo(){
                             <th>KTNs Removed</th>
                             <th>Removed By</th>
                             <th>Date</th>
+                            {currentUserRole === "admin" && <th>#</th>}
                         </tr>
                     </thead>
                     <tbody>
                         {removed.map(r => (
                             <tr key={r.id}>
-                                <td>{r.bill_no}</td>
-                                <td>{r.ktns}</td>
+                                <td>
+                                    {/* {r.bill_no} */}
+                                    {editingId === r.id ? (
+                                        <input type="number" className="form-control" value={editBill} onChange={e => setEditBill(Number(e.target.value))}/>
+                                    ) : (
+                                        r.bill_no
+                                    )}
+                                </td>
+                                <td>
+                                    {/* {r.ktns} */}
+                                    {editingId === r.id ? (
+                                        <input type="number" className="form-control" value={editKTN} onChange={e => setEditKTN(Number(e.target.value))}/>
+                                    ) : (
+                                        r.ktns
+                                    )}
+                                </td>
                                 <td>{r.user}</td>
                                 <td>{r.removedAt?.toDate().toLocaleString()}</td>
+                                {currentUserRole === "admin" && <td>
+                                    {editingId === r.id ? (
+                                        <CheckIcon
+                                            style={{ cursor: "pointer" }}
+                                            color="success"
+                                            onClick={() => saveEdit(r.id)}
+                                        />
+                                    ) : (
+                                        <EditIcon
+                                            style={{ cursor: "pointer" }}
+                                            color="primary"
+                                            onClick={() => startEdit(r)}
+                                        />
+                                    )}
+                                </td>}
                             </tr>
                         ))}
                     </tbody>
